b23bb9f0c023007f79bfc3d3dc3e7d232323dde7
juniorinnover <juniorinnover@outlook.com>
2020-07-16 17:31:23 -0500

    AVANCE 4

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 let cv = window.document.getElementById('cv');
 let ctx = cv.getContext('2d');
-
 // let lastTime = new Date().getTime(), currentFPS = 0, frameCount = 0;
+
+cv.width = 292;
+cv.height = 292;
 
 let kirby = new Image();
 
 kirby.src = 'img/standby.png';
-
 kirby.addEventListener('load', loop, false);
+
+let numCol = 16;
+let numFil = 2;
+let widthKirby;
+let heightKirby;
 
 function loop()
 {
+	widthKirby = kirby.width / numCol;
+	heightKirby = kirby.height / numFil;
+
 	draw();
 	requestAnimationFrame(loop);
 }
@@ -19,6 +28,8 @@ function draw()
 {
 	cv.width = cv.width;
 	cv.height = cv.height;
+
+	console.log(widthKirby + ' x ' + heightKirby);
 
 	ctx.drawImage(kirby, 0 , 0);
 }
