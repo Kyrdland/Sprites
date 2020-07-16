2c59289acec453b42b19a502cfc696fd5b1cea28
VictorMiBe <59127395+VictorMiBe@users.noreply.github.com>
2020-07-16 18:12:09 -0500

    Kirby a todo gas

--- main.js
+++ main.js
@@ -3,14 +3,17 @@ let ctx = cv.getContext('2d');
 
 let kirby = new Image();
 kirby.src = 'img/standby.png'
-kirby.addEventListener('load', loop,false);
+kirby.addEventListener('load', pre,false);
 
 let numCol = 16;
 let numFil = 2;
 let widthKirby;
 let heightKirby;
 
-let lastTime = new Date().getTime(); 1
+let frameKirbyX = 0;
+let frameKirbyY = 0;
+
+let lastTime = new Date().getTime(); 
 let currentFPS = 0; 
 let frameCount = 0;
 
@@ -25,16 +28,27 @@ function pre()
 
 function loop()
 {
+    update();
     draw();
     //updateFPS();
     //console.log(currentFPS);
     window.requestAnimationFrame(loop)
 }
 
+function update()
+{
+    frameKirbyX++;
+    frameKirbyY++;
+    frameKirbyX = frameKirbyX % numCol;
+    frameKirbyY = frameKirbyY % numFil;
+}
+
 function draw()
 {
-    console.log(widthKirby + ' x ' + heightKirby);
-    ctx.drawImage(kirby, 0, 0);
+    cv.width = cv.width;
+    cv.height = cv.height;
+    // console.log(widthKirby + ' x ' + heightKirby);
+    ctx.drawImage(kirby, (frameKirbyX * widthKirby), 0,kirby.width,kirby.height,0,0,kirby.width,kirby.height);
 }
 // function updateFPS()
 // {
