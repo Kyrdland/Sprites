b067c758915fa1d6a80ccc597f2fbd77aa8becf8
VictorMiBe <59127395+VictorMiBe@users.noreply.github.com>
2020-07-16 17:25:46 -0500

    Cambios 5:25

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 let cv = document.getElementById("cv");
 let ctx = cv.getContext('2d');
 
-let lastTime = new Date().getTime(), currentFPS, frameCount;
+let kirby = new Image();
+kirby.src = 'img/standby.png'
+kirby.addEventListener('load', loop,false);
+
+let lastTime = new Date().getTime(); 
+let currentFPS = 0; 
+let frameCount = 0;
 
 function loop()
 {
+    draw();
+    //updateFPS();
+    //console.log(currentFPS);
     window.requestAnimationFrame(loop)
-    updateFPS();
-    console.log(currentFPS);
 }
 
-function updateFPS()
+function draw()
 {
-    let initTime = new Date().getTime();
-    if(initTime - lastTime > 1000)
-    {
-        currentFPS = frameCount; 
-        frameCount = 0;
-        lastTime = initTime;
-    }
-    frameCount++;
+    cv.width =cv.width;
+    cv.height = cv.height;
+    ctx.drawImage(kirby, 0, 0);
 }
+// function updateFPS()
+// {
+//     let initTime = new Date().getTime();
+//     if(initTime - lastTime > 1000)
+//     {
+//         currentFPS = frameCount; 
+//         frameCount = 0;
+//         lastTime = initTime;
+//     }
+//     frameCount++;
+// }
 
-loop();
+// loop();
