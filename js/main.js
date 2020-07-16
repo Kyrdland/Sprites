0af79b4f64bab19050b0a3f52a50ad5b402e8137
juniorinnover <juniorinnover@outlook.com>
2020-07-16 18:19:46 -0500

    AVANCE 6

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 let cv = window.document.getElementById('cv');
 let ctx = cv.getContext('2d');
-// let lastTime = new Date().getTime(), currentFPS = 0, frameCount = 0;
-
-
 
 let kirby = new Image();
 
-kirby.src = 'img/standby.png';
+kirby.src = 'img/run.png';
 kirby.addEventListener('load', pre, false);
 
-let numCol = 16;
+let numCol = 8;
 let numFil = 2;
 let widthKirby;
 let heightKirby;
@@ -20,8 +17,8 @@ function pre()
 {
 	widthKirby = kirby.width / numCol;
 	heightKirby = kirby.height / numFil;
-	cv.width = widthKirby;
-	cv.height = heightKirby;
+	cv.width = 1000;
+	cv.height = 1000;
 	loop();
 }
 
@@ -47,19 +44,5 @@ function draw()
 	cv.width = cv.width;
 	cv.height = cv.height;
 	// image, ix, iy, iwidth, iheight, cx, cy, cwidth, cheight
-	ctx.drawImage(kirby, (frameKirbyX * widthKirby), 0, widthKirby, heightKirby, 0, 0, widthKirby, heightKirby);
+	ctx.drawImage(kirby, (frameKirbyX * widthKirby), 0, widthKirby, heightKirby, 300, 0, widthKirby, heightKirby);
 }
-
-// function updateFPS()
-// {
-// 	let iniTime = new Date().getTime();
-
-// 	if(iniTime - lastTime > 1000)
-// 	{
-// 		currentFPS = frameCount;
-// 		frameCount = 0;
-// 		lastTime = iniTime;
-// 	}
-
-// 	frameCount++;
-// }
